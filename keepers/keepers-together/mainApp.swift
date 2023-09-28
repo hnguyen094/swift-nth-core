7da2ed7b5bad11d65be4076245d4de3332b09c5d
Hung Nguyen <hungnguyen094@gmail.com>
2023-09-28 01:27:52 -0500

    removed direct swiftdata usage

--- keepers/keepers-together/mainApp.swift
+++ keepers/keepers-together/mainApp.swift
@@ -6,29 +6,16 @@
 //
 
 import SwiftUI
-import SwiftData
+import ComposableArchitecture
 
 @main
 struct mainApp: App {
-    private let sharedModelContainer: ModelContainer
-    
-    init() {
-        do {
-            sharedModelContainer = try Shared.createModelContainer()
-        } catch {
-            fatalError("Failed to load the model container (maybe CloudKit configuration error?) \(error)")
-        }
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
-
         ImmersiveSpace(id: "ImmersiveSpace") {
             ImmersiveView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }

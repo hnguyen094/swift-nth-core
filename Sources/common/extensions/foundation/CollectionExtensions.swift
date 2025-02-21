7ef03ba043d452620b41edd3d86ee0ac569ea05d
Hung Nguyen <hungnguyen094@gmail.com>
2025-02-21 03:54:55 +0000

    safe collection indexing

---
+++ Sources/common/extensions/foundation/CollectionExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionExtensions.swift
+//  swift-nth-core
+//
+//  Created by Hung Nguyen on 2/21/25.
+//
+
+import Foundation
+
+public extension Collection {
+    // Returns the element at the specified index if it is within bounds, otherwise nil.
+
+    /**
+     Returns the element at the specified index if it is within bounds, otherwise nil.
+     - Author:  [Nikita Kukushkin](https://stackoverflow.com/a/30593673)
+     */
+    subscript(safe index: Index) -> Element? {
+        indices.contains(index) ? self[index] : nil
+    }
+}

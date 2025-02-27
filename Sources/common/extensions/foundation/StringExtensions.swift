ab426bb6c747abb8910da4e114a14e26e9748310
Hung Nguyen <hungnguyen094@gmail.com>
2025-02-27 16:04:19 +0000

    removed capitalizingfirstletter for swift-native implementation

--- Sources/common/extensions/foundation/StringExtensions.swift
+++ Sources/common/extensions/foundation/StringExtensions.swift
@@ -9,12 +9,4 @@ import Foundation
 
 public extension String {
     static let zwsp: Self = "​"
-    
-    func capitalizingFirstLetter() -> String {
-        return prefix(1).uppercased() + self.lowercased().dropFirst()
-    }
-    
-    mutating func capitalizeFirstLetter() {
-        self = self.capitalizingFirstLetter()
-    }
 }

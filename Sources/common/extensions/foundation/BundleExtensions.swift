6821c61958d43426eb70a2baf907aedf98eb53f2
Hung Nguyen <hungnguyen094@gmail.com>
2025-04-07 12:44:39 +0100

    add bundle extension for app version

---
+++ Sources/common/extensions/foundation/BundleExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  BundleExtensions.swift
+//  swift-nth-core
+//
+//  Created by Hung Nguyen on 4/7/25.
+//
+
+import Foundation
+
+public extension Bundle {
+    static var appVersion: String? {
+        #if os(macOS)
+        let key = "CFBundleShortVersionString"
+        #else
+        let key = "CFBundleVersion"
+        #endif
+        return Bundle.main.object(forInfoDictionaryKey: key) as? String
+    }
+}
